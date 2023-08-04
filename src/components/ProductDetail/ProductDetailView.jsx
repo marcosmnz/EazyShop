5ce4d519120d2506d3750f951ce813d2bc010324
Marcos Muñoz <marco_munoz2401@hotmail.com>
2023-08-04 03:35:04 -0500

    EazyShop V1.0.2

--- src/components/ProductDetail/ProductDetailView.jsx
+++ src/components/ProductDetail/ProductDetailView.jsx
@@ -21,30 +21,30 @@ function ProductDetailView({ data, addProduct}) {
         theme="light"
       />
       <div className="is-flex is-flex-wrap-wrap card main-container columns is-flex-mobile is-flex-tablet is-flex mx-6 my-3">
-        <div className="column is-2">
+        <div className="column ">
         <div className="card-image has-background-white is-justify-content-center">
             <figure className="image is-square">
               <img src={data.image} alt={data.title} />
             </figure>
           </div>
         </div>
-        <div className="column is-flex is-flex-flex-wrap">
+        <div className="card-content column is-flex is-flex-flex-wrap">
           <div className="card-content">
             <div className="media">
               <div className="media-content">
-                <p className="card-title title is-4">{data.title}</p>
+                <p className="title is-4">{data.title}</p>
               </div>
             </div>
 
             <div className="content is-flex-wrap-nowrap">
-              <div className="card-text">
+              <div className="">
               {data.description}
               </div>
               <div className="columns is-flex is-flex-wrap-wrap mt-6 pt-6">
-                  <div className="column is-3 is-flex is-justify-content-center is-align-items-center">
+                  <div className="column m-2 is-flex is-justify-content-center is-align-items-center">
                     <strong className="is-size-3">${data.price}</strong>
                   </div>
-                  <div className="column is-flex is-justify-content-center is-align-items-center is-half">
+                  <div className="column m-2 is-flex is-justify-content-center is-align-items-center">
                     <button onClick={() => addProduct(data)} className="button is-size-4 is-warning">
                       <i className="fa-solid fa-cart-arrow-down"></i>Add to cart
                     </button>
